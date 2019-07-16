9e2b7970a2be0fe0298ccc04d710b333d2fffc81
PietroLiuzzo <pietro.liuzzo@gmail.com>
2019-07-16 15:29:28 +0200

    updated registry and select population

--- register.js
+++ register.js
@@ -1,15 +1,16 @@
 
 $( document ).ready(function() {
-$.getJSON('register.json', function (data) {
+$.getJSON('https://raw.githubusercontent.com/distributed-text-services/reader/master/register.json', function (data) {
     console.log(data)
-    
-    for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        
+    $(data.endpoints).each(function( index, element ) {
         var select = $('#RegisteredApi')
+        select.append('<option value="' + element.endpoint + '">' + element.label + ' (' + element.status + ')</option>')
+        });
+  /*  for (var i = 0; i < data.length; i++) {
+        var obj = data[i];
+        console.log(obj)
         
-        select.append('<option value="' + obj.endpoint + '">' + obj.label + ' (' + obj.status + ')</option>')
-    }
+    }*/
 });
 
 });
