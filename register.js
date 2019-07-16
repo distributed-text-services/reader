8efce2d0db3738d17c39bcf1b621b0cb5d4efff5
PietroLiuzzo <pietro.liuzzo@gmail.com>
2019-07-16 15:33:14 +0200

    removed logging

--- register.js
+++ register.js
@@ -1,16 +1,10 @@
 
 $( document ).ready(function() {
 $.getJSON('https://raw.githubusercontent.com/distributed-text-services/reader/master/register.json', function (data) {
-    console.log(data)
     $(data.endpoints).each(function( index, element ) {
         var select = $('#RegisteredApi')
         select.append('<option value="' + element.endpoint + '">' + element.label + ' (' + element.status + ')</option>')
         });
-  /*  for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        console.log(obj)
-        
-    }*/
-});
+  });
 
 });
